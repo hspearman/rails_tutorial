aba962790c8318c5bc6efc67424a978c9a4217c7
Hannah Spearman <hspearman92@gmail.com>
2017-05-08 13:06:16 -0500

    Added views / actions for comments resource (nested in articles resource)

--- blog/app/controllers/articles_controller.rb
+++ blog/app/controllers/articles_controller.rb
@@ -1,67 +1,67 @@
 class ArticlesController < ApplicationController
 
-    public
+public
 
-        def index
-            @articles = Article.all
+    def index
+        @articles = Article.all
+    end
+
+    def show
+        @article = Article.find(params[:id])
+    end
+
+    def new
+        @article = Article.new
+    end
+
+    def edit
+        @article = Article.find(params[:id])
+    end
+
+    def create
+
+        # Create article from parameters
+        @article = Article.new(article_params)
+
+        # Save article (if applicable)
+        if @article.save
+            redirect_to @article
+        else
+            render 'new'
         end
 
-        def show
-            @article = Article.find(params[:id])
+    end
+
+    def update
+
+        # Find article by ID
+        @article = Article.find(params[:id])
+
+        # Update article (if applicable)
+        if @article.update(article_params)
+            redirect_to @article
+        else
+            render 'edit'
         end
 
-        def new
-            @article = Article.new
-        end
+    end
 
-        def edit
-            @article = Article.find(params[:id])
-        end
+    def destroy
+        
+        # Find article by ID
+        @article = Article.find(params[:id])
 
-        def create
+        # Delete article
+        @article.destroy
 
-            # Create article from parameters
-            @article = Article.new(article_params)
+        redirect_to articles_path
 
-            # Save article (if applicable)
-            if @article.save
-                redirect_to @article
-            else
-                render 'new'
-            end
+    end
 
-        end
+private
 
-        def update
-
-            # Find article by ID
-            @article = Article.find(params[:id])
-
-            # Update article (if applicable)
-            if @article.update(article_params)
-                redirect_to @article
-            else
-                render 'edit'
-            end
-
-        end
-
-        def destroy
-            
-            # Find article by ID
-            @article = Article.find(params[:id])
-
-            # Delete article
-            @article.destroy
-
-            redirect_to articles_path
-
-        end
-
-    private
-
-        def article_params
-            params.require(:article).permit(:title, :text)
-        end
+    def article_params
+        params.require(:article).permit(:title, :text)
+    end
 
 end
---
+++ blog/app/controllers/comments_controller.rb
@@ -0,0 +1,19 @@
+class CommentsController < ApplicationController
+
+public
+    def create
+
+        # Create comment on article
+        @article = Article.find(params[:article_id])
+        @comment = @article.comments.create(comment_params)
+
+        redirect_to article_path(@article)
+    end
+
+private
+
+    def comment_params
+        params.require(:comment).permit(:commenter, :body)
+    end
+
+end
