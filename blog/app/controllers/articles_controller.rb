a2447392e63fcc6017cc1d659fb9e071998c5368
Hannah Spearman <hspearman92@gmail.com>
2017-05-08 11:26:13 -0500

    Added validation to articles creation

--- blog/app/controllers/articles_controller.rb
+++ blog/app/controllers/articles_controller.rb
@@ -9,15 +9,23 @@ class ArticlesController < ApplicationController
     end
 
     def new
+        @article = Article.new
     end
 
     def create
+
+        # Create article from parameters
         @article = Article.new(params
             .require(:article)
             .permit(:title, :text))
 
-        @article.save
-        redirect_to @article
+        # Save article (if applicable)
+        if @article.save
+            redirect_to @article
+        else
+            render 'new'
+        end
+
     end
 
 end
